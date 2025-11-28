69e54fe9a544c261bb50b87073723df91960c9c4
Gábor <gabor.roszjar@gmail.com>
2025-11-28 08:58:14 +0100

    Initial commit

---
+++ src/main/java/hu/rgabor/dwb/DwbHomeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package hu.rgabor.dwb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwbHomeAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwbHomeAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/rgabor/dwb/DwbHomeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.rgabor.dwb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwbHomeAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
