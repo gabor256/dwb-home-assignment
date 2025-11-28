1d909159dafebb9f86de597c5641c29a0eeeb8ff
Gábor <gabor.roszjar@gmail.com>
2025-11-28 09:20:11 +0100

    Define specific endpoint

--- src/main/java/hu/rgabor/dwb/controller/DemoController.java
+++ src/main/java/hu/rgabor/dwb/controller/DemoController.java
@@ -4,17 +4,19 @@ import hu.rgabor.dwb.service.DemoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/calculate")
 public class DemoController {
 
     private final DemoService demoService;
 
-    @PostMapping
+    @PostMapping("/a")
     public int[] calculateResult(@RequestParam int[] input) {
         log.debug("Calculate demo output. Received input: {}", input);
         return demoService.calculateResult(input);
